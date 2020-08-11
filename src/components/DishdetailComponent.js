9ed958c5616125bb50f93994be3400c4e21f707d
Arjun Dogra <arjundogra146@gmail.com>
2020-08-11 23:02:41 +0530

    assignment 1

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -10,23 +10,67 @@ class DishDetail extends Component {
         }
     }
 
-    render(){
-        const dish = (this.props.dishh);
-        console.log(this.props.dishh);
+    renderDish(dish) {
         if (dish != null)
-                return(
-                    <Card>
-                        <CardImg top src={dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                );
-            else
-                return(
-                    <div></div>
-                );
+            return(
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
+
+    renderComments(dish){
+        if(dish != null){
+            let comments = dish.comments;
+            console.log(comments[0].comment)
+            return(
+                <ul className="list-unstyled">
+                <li>{comments[0].comment}</li>
+                <li className="list-inline-item">--{comments[0].author}</li>
+                <li className="list-inline-item">{comments[0].date}</li>
+                <li>{comments[1].comment}</li>
+                <li className="list-inline-item">--{comments[1].author}</li>
+                <li className="list-inline-item">{comments[1].date}</li>
+                <li>{comments[2].comment}</li>
+                <li className="list-inline-item">--{comments[2].author}</li>
+                <li className="list-inline-item">{comments[2].date}</li>
+                <li>{comments[3].comment}</li>
+                <li className="list-inline-item">--{comments[3].author}</li>
+                <li className="list-inline-item">{comments[3].date}</li>
+                <li>{comments[4].comment}</li>
+                <li className="list-inline-item">--{comments[4].author}</li>
+                <li className="list-inline-item">{comments[4].date}</li>
+                </ul>
+            )
+        }
+        else{
+            return(
+                <div></div>
+            );
+        }
+    }
+
+    render(){
+        console.log(this.props.dishh);
+            return(
+                <div className="row">
+                    <div className="col-md-5 col-12 m-1">
+                        {this.renderDish(this.props.dishh)}
+                    </div>
+                    <div className="col-md-5 col-12 m-1">
+                        <h4>Comments</h4>
+                        {this.renderComments(this.props.dishh)}
+                    </div>
+                </div>
+            );
     }
 }
 
