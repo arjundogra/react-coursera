85474d9f432e403e21c0bb9e0ff01744307ea5a7
Arjun Dogra <arjundogra146@gmail.com>
2020-08-13 20:21:55 +0530

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -27,50 +27,45 @@ class DishDetail extends Component {
             );
     }
 
-    renderComments(dish){
-        if(dish != null){
-            let comments = dish.comments;
-            console.log(comments[0].comment)
+    renderComments(comments){
+            var commentList = comments.map(comment => {
+                return (
+                    <li key={comment.id}>
+                        <p>{comment.comment}</p>
+                        <p className="list-inline-item">--{comment.author}</p>
+                        <p className="list-inline-item">{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                    </li>
+                );
+            });
             return(
                 <div className="col-md-5 col-12 m-1">
                     <h4>Comments</h4>
-                    <ul className="list-unstyled">
-                        <p>{comments[0].comment}</p>
-                        <p className="list-inline-item">--{comments[0].author}</p>
-                        <p className="list-inline-item">{comments[0].date}</p>
-                        <p>{comments[1].comment}</p>
-                        <p className="list-inline-item">--{comments[1].author}</p>
-                        <p className="list-inline-item">{comments[1].date}</p>
-                        <p>{comments[2].comment}</p>
-                        <p className="list-inline-item">--{comments[2].author}</p>
-                        <p className="list-inline-item">{comments[2].date}</p>
-                        <p>{comments[3].comment}</p>
-                        <p className="list-inline-item">--{comments[3].author}</p>
-                        <p className="list-inline-item">{comments[3].date}</p>
-                        <p>{comments[4].comment}</p>
-                        <p className="list-inline-item">--{comments[4].author}</p>
-                        <p className="list-inline-item">{comments[4].date}</p>
+                    <ul className="">
+                        {commentList}
                     </ul>
                 </div>
             )
-        }
+    };
+
+    render(){
+        console.log(this.props.dish);
+        if (this.props.dish != null){    
+            return(
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-5 col-12 m-1">
+                                {this.renderDish(this.props.dish)}
+                            </div>
+                                {this.renderComments(this.props.dish.comments)}
+                        </div>
+                    </div>
+                );
+            }
         else{
             return(
                 <div></div>
-            );
-        }
-    }
-
-    render(){
-        console.log(this.props.dishh);
-            return(
-                <div className="row">
-                    <div className="col-md-5 col-12 m-1">
-                        {this.renderDish(this.props.dishh)}
-                    </div>
-                    {this.renderComments(this.props.dishh)}
-                </div>
-            );
+            )
+        }            
     }
 }
 
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -7,23 +7,15 @@ class Menu extends Component {
   constructor(props) {
       super(props);
 
-      this.state = {
-          selectedDish: null
-      }
-  }
-
-  onDishSelect(dish) {
-      this.setState({ selectedDish: dish});
   }
 
   render() {
-      console.log(this.state.selectedDish)
       console.log(this.props.dishes)
       const menu = this.props.dishes.map((dish) => {
           return (
             <div  className="col-12 col-md-5 m-1">
               <Card key={dish.id}
-                onClick={() => this.onDishSelect(dish)}>
+                        onClick={() => this.props.onClick(dish.id)}>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
@@ -38,7 +30,6 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                <DishDetail dishh={this.state.selectedDish} />
         </div>
       );
   }
